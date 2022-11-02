f387f425e4d7ebe77c5a5389990b511876d4a275
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-11-01 21:59:58 -0500

    cleaned up navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,32 +2,14 @@ import React from 'react';
 
 function Header() {
     return (
-        <div className="jumbotron jumbotron-fluid bg-primary d-flex justify-content-around">
-            <div className="container row d-flex justify-content-around">
+        <div className="jumbotron jumbotron-fluid row bg-dark text-white">
+            <div className="container row d-flex">
                 <h1 className="display-4 col-lg-4">Brendon Jiang</h1>
-                <nav className="navbar navbar-expand-lg bg-light col-lg-8">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">About Me</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Portfolio</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Contact</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href='#'>Resume</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                <nav className="navbar navbar-expand-lg bg-dark col-lg-8 justify-content-center">
+                    <a className="navbar-brand text-white text-decoration-none" href="#about-me">About Me</a>
+                    <a className="navbar-brand text-white text-decoration-none" href="#portfolio">Portfolio</a>
+                    <a className="navbar-brand text-white text-decoration-none" href="#contact">Contact</a>
+                    <a className="navbar-brand text-white text-decoration-none" href='#resume'>Resume</a>
                 </nav>
             </div>
         </div>
