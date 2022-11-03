35d3a91964623390411cc8ebaf7bae40904796af
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-11-02 22:35:58 -0500

    added content to resume page

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -5,18 +5,24 @@ export default function Resume() {
         <div className='vh-100 text-white container'>
             <h1 className=' d-flex justify-content-center'>Resume</h1>
             <div className='container-fluid col-6'>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-                velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-                ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-                non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-                ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-                rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-                tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-                porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-                vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-                fames ac ante ipsum primis in faucibus.
-            </p>
+            <h3>Front-end Proficiencies</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>JQuery</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </ul>
+            <h3>Back-end Proficiencies</h3>
+            <ul>
+                <li>API</li>
+                <li>NodeJS</li>
+                <li>Express</li>
+                <li>MySQL/Sequelize</li>
+                <li>MongoDB/Mongoose</li>
+                <li>GraphQL</li>
+            </ul>
             </div>
         </div>
     )
